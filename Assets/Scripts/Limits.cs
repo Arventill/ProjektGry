b5d9bc0b31d99070215bb4fd1844b6505a7dd5dc
Arventilll <filip.nowicki1999@gmail.com>
2021-01-06 11:57:25 +0100

    PGD-003 Post fix repairing

---
+++ Assets/Scripts/Limits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the hinge limits for a door.
+        HingeJoint hinge = GetComponent<HingeJoint>();
+
+        JointLimits limits = hinge.limits;
+        limits.min = -86;
+        limits.bounciness = 0;
+        limits.bounceMinVelocity = 0;
+        limits.max = 130;
+        hinge.limits = limits;
+        hinge.useLimits = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ObjectsAnimate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsAnimate : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            animator.StartPlayback();
+            //animator.gameObject.SetActive(true);
+        }
+    }
+}
