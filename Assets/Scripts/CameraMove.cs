cc846c0553aadfc501ad74c45e8f137bdad4929a
Arventilll <filip.nowicki1999@gmail.com>
2021-01-11 11:23:10 +0100

    PGD-005 Add test version of code

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float y = 0.0f;
+    public float TestCamPlusY;
+    public float actualRotation;
+    public float cameraSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y = -1 * Input.GetAxisRaw("Mouse Y") * cameraSpeed * Time.deltaTime;
+        
+        TestCamPlusY = transform.rotation.x + y;
+        actualRotation = this.transform.rotation.x;
+        
+
+
+        //if (transform.rotation.x + y > 0.40f && transform.rotation.x + y < -0.30f)
+        {
+            transform.Rotate(y, 0, 0);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public float mouseSens = 5000f;
-    public Transform playerBody;
-
-    private float _vRotation = 0f;
+    public float cameraSpeed = 10.0f;
+    public Vector3 Test;
+    public Vector3 Test2;
+    
+    private Vector3 motion;
     private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
@@ -23,29 +23,28 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        // Moving camera with mouse
+        motion = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0);// Input.GetAxisRaw("Mouse Y"));
+        Test = motion;
+        Test2 = motion * cameraSpeed * Time.deltaTime;
 
+        transform.Rotate(motion * cameraSpeed * Time.deltaTime);
+
+        //Moving camera with WSAD
+        //motion = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        //_rigidbody.velocity = motion * cameraSpeed;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        float translationWS = Input.GetAxis("Vertical") * speed;
-        float translationAD = Input.GetAxis("Horizontal") * speed;
+        float translationWS = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float translationAD = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(translationAD, 0, translationWS);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             transform.Translate(0, 5, 0);
         }
-        float rotation = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translationWS *= Time.deltaTime;
-        translationAD *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(translationAD, 0, translationWS);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, rotation, 0);
     }
 }
